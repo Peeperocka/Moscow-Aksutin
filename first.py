72061d43c5fe52fdef83bee3a63a2c59aa77a9ab
Peeper <Peeperocka@yandex.ru>
2024-11-28 18:45:05 +0300

    2 урок из классной работы

--- first.py
+++ first.py
@@ -1,2 +1,36 @@
+import csv
+import sys
+import io
+import random
+
+from PyQt6 import uic
+from PyQt6.QtCore import QPointF
+from PyQt6.QtGui import QColor, QPainter
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class gitAndYellowCircles(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("UI.ui", self)
+        self.pushButton.clicked.connect(self.draw_circle)
+
+    def draw_circle(self):
+        self.update()
+
+    def paintEvent(self,event):
+        x = random.randint(0, self.width())
+        y = random.randint(0, self.height())
+        diameter = random.uniform(10, self.width() / 2)
+        color = QColor(255,255,0)
+
+        painter = QPainter(self)
+        painter.setBrush(color)
+        painter.drawEllipse(QPointF(x, y), diameter / 2, diameter / 2)
+
+
 if __name__ == '__main__':
-    print('My first git-repo')
+    app = QApplication(sys.argv)
+    ex = gitAndYellowCircles()
+    ex.show()
+    sys.exit(app.exec())
